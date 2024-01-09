07abb0acef5d0e3c83678405369f516a6f935926
pierpaolo.feltrin <holytuta@tutanota.com>
2024-01-09 19:14:42 +0100

    Fixed stupid vscode auto formatting

--- gol.c
+++ gol.c
@@ -13,12 +13,9 @@
 int count_live_neighbors(bool world[ROWS][COLUMNS], int row, int col)
 {
     int count = 0;
-    for (size_t i = row - 1; i <= row + 1; ++i)
-    {
-        for (size_t j = col - 1; j <= col + 1; ++j)
-        {
-            if (i >= 0 && i < ROWS && j >= 0 && j < COLUMNS && !(i == row && j == col) && world[i][j])
-            {
+    for (size_t i = row - 1; i <= row + 1; ++i){
+        for (size_t j = col - 1; j <= col + 1; ++j){
+            if (i >= 0 && i < ROWS && j >= 0 && j < COLUMNS && !(i == row && j == col) && world[i][j]){
                 count++;
             }
         }
@@ -48,14 +45,11 @@ void create_world()
 
     bool world[ROWS][COLUMNS] = {false};
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()){
         BeginDrawing();
         {
-            if ( IsKeyDown(KEY_D) && IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
-            {
-                for (size_t i = 0, j = 0; i < COLUMNS && j < ROWS; ++i, ++j)
-                {
+            if (IsKeyDown(KEY_D) && IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
+                for (size_t i = 0, j = 0; i < COLUMNS && j < ROWS; ++i, ++j){
                     int mouseY = i * RECT_DIM;
                     int mouseX = j * RECT_DIM;
                     world[i][j] = true;
@@ -63,10 +57,8 @@ void create_world()
                 }
             }
 
-            if (IsKeyDown(KEY_D) && IsMouseButtonReleased(MOUSE_BUTTON_RIGHT))
-            {
-                for (size_t i = COLUMNS - 1, j = 0; i >= 0 && j < ROWS; --i, ++j)
-                {
+            if (IsKeyDown(KEY_D) && IsMouseButtonReleased(MOUSE_BUTTON_RIGHT)){
+                for (size_t i = COLUMNS - 1, j = 0; i >= 0 && j < ROWS; --i, ++j){
                     int mouseY = i * RECT_DIM;
                     int mouseX = j * RECT_DIM;
                     world[i][j] = true;
@@ -75,12 +67,10 @@ void create_world()
             }
 
             // Draw horizontal line
-            if (IsKeyDown(KEY_H) && IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
-            {
+            if (IsKeyDown(KEY_H) && IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
                 int mouseY = GetMouseY() % ROWS * RECT_DIM;
 
-                for (size_t i = 0; i < COLUMNS; ++i)
-                {
+                for (size_t i = 0; i < COLUMNS; ++i){
                     int mouseX = i * RECT_DIM;
                     world[mouseY / RECT_DIM][i] = true;
                     DrawRectangle(mouseX, mouseY, RECT_DIM, RECT_DIM, RAYWHITE);
@@ -88,12 +78,10 @@ void create_world()
             }
 
             // Draw vertical line
-            if (IsKeyDown(KEY_V) && IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
-            {
+            if (IsKeyDown(KEY_V) && IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
                 int mouseX = GetMouseX() % COLUMNS * RECT_DIM;
 
-                for (size_t i = 0; i < ROWS; ++i)
-                {
+                for (size_t i = 0; i < ROWS; ++i){
                     int mouseY = i * RECT_DIM;
                     world[i][mouseX / RECT_DIM] = true;
                     DrawRectangle(mouseX, mouseY, RECT_DIM, RECT_DIM, RAYWHITE);
@@ -101,8 +89,7 @@ void create_world()
             }
 
             // draw freely
-            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-            {
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
                 int mouseX = GetMouseX() % COLUMNS * RECT_DIM;
                 int mouseY = GetMouseY() % ROWS * RECT_DIM;
                 world[mouseY / RECT_DIM][mouseX / RECT_DIM] = true;
@@ -110,8 +97,7 @@ void create_world()
             }
 
             // erase
-            if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
-            {
+            if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)){
                 int mouseX = GetMouseX() % COLUMNS * RECT_DIM;
                 int mouseY = GetMouseY() % ROWS * RECT_DIM;
                 world[mouseY / RECT_DIM][mouseX / RECT_DIM] = false;
@@ -119,48 +105,37 @@ void create_world()
             }
 
             // delete everything
-            if (IsKeyReleased(KEY_C))
-            {
+            if (IsKeyReleased(KEY_C)){
                 ClearBackground(BACKGROUND);
                 memset(world, 0, sizeof(world));
             }
 
             // next generation
-            if (IsKeyDown(KEY_N))
-            {
+            if (IsKeyDown(KEY_N)){
 
                 bool temp_world[ROWS][COLUMNS] = {false};
 
-                for (size_t i = 0; i < ROWS; ++i)
-                {
-                    for (size_t j = 0; j < COLUMNS; ++j)
-                    {
+                for (size_t i = 0; i < ROWS; ++i){
+                    for (size_t j = 0; j < COLUMNS; ++j){
                         int liveNeighbors = count_live_neighbors(world, i, j);
 
-                        if (world[i][j])
-                        {
-                            if (liveNeighbors == 2 || liveNeighbors == 3)
-                            {
+                        if (world[i][j]){
+                            if (liveNeighbors == 2 || liveNeighbors == 3){
                                 temp_world[i][j] = true;
                             }
-                            else
-                            {
+                            else{
                                 temp_world[i][j] = false;
                             }
                         }
-                        else
-                        {
-                            if (liveNeighbors == 3)
-                            {
+                        else{
+                            if (liveNeighbors == 3){
                                 temp_world[i][j] = true;
                             }
                         }
-                        if (temp_world[i][j])
-                        {
+                        if (temp_world[i][j]){
                             DrawRectangle(j * RECT_DIM, i * RECT_DIM, RECT_DIM, RECT_DIM, RAYWHITE);
                         }
-                        else
-                        {
+                        else{
                             DrawRectangle(j * RECT_DIM, i * RECT_DIM, RECT_DIM, RECT_DIM, BACKGROUND);
                         }
                     }
@@ -182,40 +157,31 @@ void show_world(void (*init_function)(bool world[ROWS][COLUMNS]), bool world[ROW
     InitWindow(WIDTH, HEIGHT, "Game of Life");
     SetTargetFPS(60);
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()){
         BeginDrawing();
         {
             ClearBackground(BACKGROUND);
 
             bool temp_world[ROWS][COLUMNS] = {false};
 
-            for (size_t i = 0; i < ROWS; ++i)
-            {
-                for (size_t j = 0; j < COLUMNS; ++j)
-                {
+            for (size_t i = 0; i < ROWS; ++i){
+                for (size_t j = 0; j < COLUMNS; ++j){
                     int liveNeighbors = count_live_neighbors(world, i, j);
 
-                    if (world[i][j])
-                    {
-                        if (liveNeighbors == 2 || liveNeighbors == 3)
-                        {
+                    if (world[i][j]){
+                        if (liveNeighbors == 2 || liveNeighbors == 3){
                             temp_world[i][j] = true;
                         }
-                        else
-                        {
+                        else{
                             temp_world[i][j] = false;
                         }
                     }
-                    else
-                    {
-                        if (liveNeighbors == 3)
-                        {
+                    else{
+                        if (liveNeighbors == 3){
                             temp_world[i][j] = true;
                         }
                     }
-                    if (temp_world[i][j] == true)
-                    {
+                    if (temp_world[i][j] == true){
                         DrawRectangle(j * RECT_DIM, i * RECT_DIM, RECT_DIM, RECT_DIM, RAYWHITE);
                     }
                 }
@@ -235,41 +201,19 @@ int main(int argc, char **argv)
     char *program = *argv++;
 
     char *command = *argv++;
-    if (command == NULL)
-    {
+    if (command == NULL){
         print_usage(program);
         return 0;
     }
 
     bool world[ROWS][COLUMNS] = {false};
-    if (strcmp(command, "block") == 0)
-    {
-        show_world(init_world_as_block, world);
-    }
-    else if (strcmp(command, "glider") == 0)
-    {
-        show_world(init_world_as_glider, world);
-    }
-    else if (strcmp(command, "blinker") == 0)
-    {
-        show_world(init_world_as_blinker, world);
-    }
-    else if (strcmp(command, "pulsar") == 0)
-    {
-        show_world(init_world_as_pulsar, world);
-    }
-    else if (strcmp(command, "gun") == 0)
-    {
-        show_world(init_world_as_glider_gun, world);
-    }
-    else if (strcmp(command, "random") == 0)
-    {
-        show_world(init_world_randomly, world);
-    }
-    else if (strcmp(command, "create") == 0)
-    {
-        create_world();
-    }
+    if (strcmp(command, "block") == 0) show_world(init_world_as_block, world);
+    else if (strcmp(command, "glider") == 0) show_world(init_world_as_glider, world);
+    else if (strcmp(command, "blinker") == 0) show_world(init_world_as_blinker, world);
+    else if (strcmp(command, "pulsar") == 0) show_world(init_world_as_pulsar, world);
+    else if (strcmp(command, "gun") == 0) show_world(init_world_as_glider_gun, world);
+    else if (strcmp(command, "random") == 0) show_world(init_world_randomly, world);
+    else if (strcmp(command, "create") == 0) create_world();
     else
     {
         printf("INVALID COMMAND: %s\n", command);
